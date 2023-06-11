a90efbdbcb77c18d17e4130c652e6a0739ae1bf8
Uzman Shafi <uzmanshafifassy@gmail.com>
2023-06-11 19:07:47 +1000

    created perfect grid

--- AI cat and mouse game/Assets/Scripts/GridMap.cs
+++ AI cat and mouse game/Assets/Scripts/GridMap.cs
@@ -10,12 +10,14 @@ public class GridMap
     private float cellSize;
     private Vector3 originPosition;
     private int[,] gridArray;
+    private TextMesh[,] debugTextArray;
     public GridMap(int width, int height, float cellSize, Vector3 originPosition)
     {
         this.width = width;
         this.height = height;
         this.cellSize = cellSize;
         this.originPosition = originPosition;
+        debugTextArray = new TextMesh[width, height];
 
         CreateGrid();
     }
@@ -28,7 +30,7 @@ public class GridMap
         {
             for (int y = 0; y < gridArray.GetLength(1); y++)
             {
-                UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * 0.5f, 8, Color.white, TextAnchor.MiddleCenter);
+                debugTextArray[x,y] = UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * 0.5f, 8, Color.white, TextAnchor.MiddleCenter);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
             }
@@ -53,5 +55,47 @@ public class GridMap
 
         // Recreate the grid
         CreateGrid();
+    }
+
+    private void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        // Convert world position to grid position
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
+    }
+
+    public void SetValue(int x, int y, int value)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            gridArray[x, y] = value;
+            debugTextArray[x, y].text = gridArray[x, y].ToString();
+        }
+    }
+
+    public void SetValue(Vector3 worldPosition, int value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
+    }
+
+    public int GetValue(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            return gridArray[x, y];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public int GetValue(Vector3 worldPosition)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetValue(x, y);
     }
 }
--- AI cat and mouse game/Assets/Scripts/GridMapGround.cs
+++ AI cat and mouse game/Assets/Scripts/GridMapGround.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-
+using CodeMonkey.Utils;
 public class GridMapGround : MonoBehaviour
 {
     public Tilemap tilemap; // Assign your tilemap in the inspector
-
+    public GridMap gridMap;
     void Start()
     {
         BoundsInt bounds = tilemap.cellBounds;
@@ -20,6 +20,18 @@ public class GridMapGround : MonoBehaviour
 
         Vector3 originPosition = transform.position - new Vector3(width * cellSize, height * cellSize) / 2f;
         
-        GridMap gridMap = new GridMap(width, height, cellSize, originPosition);     
+        GridMap(width, height, cellSize, originPosition);     
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gridMap.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(gridMap.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
     }
 }
